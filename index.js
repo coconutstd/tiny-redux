69203f493e962dc65a2c39bf6a3eb3407f6faf7d
junui <wnsspdl@naver.com>
2021-05-04 00:28:14 +0900

    redux 구현 3단계 (pub,sub 모델)

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import { createStore } from './redux.js'
 
-function reducer(state, data) {
-    if (data.type === 'count') {
-        return { ...state, counter: data.payload.counter };
+function reducer(state, action) {
+    if (action.type === 'count') {
+        return { ...state, counter: action.payload.counter };
     }
 
     return state;
 }
 
+function listener() {
+    console.log(store.getState());
+}
+
 const store = createStore(reducer);
+
+store.subscribe(listener);
 
 store.dispatch({
     type: 'count',
--- redux.js
+++ redux.js
@@ -1,16 +1,25 @@
 export function createStore(reducer) {
     let state;
+    const handler = [];
 
-    function dispatch(data) {
-        state = reducer(state, data);
+    function dispatch(action) {
+        state = reducer(state, action);
+        handler.forEach((listener) => {
+            listener();
+        });
     }
 
     function getState() {
         return state;
     }
 
+    function subscribe(listener) {
+        handler.push(listener);
+    }
+
     return {
         dispatch,
         getState,
+        subscribe
     }
 }
